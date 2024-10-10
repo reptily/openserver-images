f84a51e95483bda034252b4547bbfbeb0c708166
Vladimir Aristarkhov <v.aristarhov@sellerexpert.ru>
2024-10-10 08:57:37 +0300

    delete

--- build-all.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-# Удаляем все образы с именем <none>
-docker rmi $(docker images -f "dangling=true" -q)
-
-docker build -t openserver-php-8.3 8.3/.
-# docker build -t openserver-php-8.3:php-8.3.11 8.3/.
